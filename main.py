6ea7a5717b081b20af85f021139a15528152e647
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-29 16:00:02 -0500

    cors pip

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "users/2")
+response = requests.get(BASE + "users/1")
 
 print(response.json())
