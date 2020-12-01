3b67016f00aec8806af42d118015c04920b34154
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-12-01 16:11:12 -0500

    response data update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ CORS(app)
 @app.route("/1", methods=["GET", "OPTIONS"])
 def get_example():
     """GET in server"""
-    response = jsonify(data={"meds" : [{"medName":"Tylenol 500 mg", "directions":"Once a day, Everyday, 60 tablets", "refill":"11/30/21","exp":"12/31/22", "image":"https://www.meijer.com/content/dam/meijer/product/0030/04/5044/90/0030045044905_2_A1C1_0600.png"}, {"medName":"Advil 200 mg", "directions":"Twice a day, before breakfast and before dinner, 60 tablets", "image":"https://images.heb.com/is/image/HEBGrocery/000231870", "refill":"11/30/21", "exp":"1/31/22"}], "name" : "Greg Cruz", "age" : "31"})
+    response = jsonify(data={"meds" : [{"medName":"Tylenol 500 mg", "directions":"Once a day, Everyday, 60 tablets", "refill":"11/30/21","exp":"12/31/22", "image":"https://www.meijer.com/content/dam/meijer/product/0030/04/5044/90/0030045044905_2_A1C1_0600.png"}, {"medName":"Advil 200 mg", "directions":"Twice a day, before breakfast and before dinner, 60 tablets", "image":"https://images.heb.com/is/image/HEBGrocery/000231870", "refill":"11/30/21", "exp":"1/31/22"}, {"medName":"Metformin 500 mg", "refill":"12/31/20", "exp":"12/31/21", "directions": "Once a day with breakfast, everyday, 30 tablets", "image":"https://res.cloudinary.com/grohealth/image/upload/$wpsize_!post-thumbnail!,w_1000,h_600,c_fill,g_auto/v1588088500/Metformin-1200x630-1.jpg"}], "name" : "Greg Cruz", "age" : "31"})
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
